b093f0c5a56159014d096ac64278a3855f2b8680
Hushng Fikrat Muhibullah <fikrat86@gmail.com>
2025-11-27 12:22:54 -0500

    Build all microservices: Update version to trigger Docker builds

--- shared/version.js
+++ shared/version.js
@@ -1,5 +1,6 @@
 // Trigger rebuild of all services
 module.exports = {
-  version: '2.0.0',
-  buildDate: new Date().toISOString()
+  version: '2.0.1',
+  buildDate: new Date().toISOString(),
+  features: ['users', 'threads', 'posts', 'interactive-dashboard']
 };
